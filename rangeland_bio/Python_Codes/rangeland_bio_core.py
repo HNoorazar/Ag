a3a079b09536e2e96b7e9f4096c24da6cbeb1440
HNoorazar <HNoorazar@users.noreply.github.com>
2024-08-13 14:26:26 -0700

    initialize bio rangeland

---
+++ rangeland_bio/Python_Codes/rangeland_bio_core.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+import time, datetime
+from pprint import pprint
+import os, os.path, sys
+import scipy
+from scipy.linalg import inv
+
+"""
+There are scipy.linalg.block_diag() and scipy.sparse.block_diag()
+"""
